28de50306e519577f81fa41020037131d2405d7d
nino cimintia <ninocimintia@gmail.com>
2024-01-27 01:11:36 +0400

    Week4

---
+++ WEEK4_IT7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WEEK4_IT7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nino Tsimintia");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
